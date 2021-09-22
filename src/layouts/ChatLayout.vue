6f10dc83e3b1ad827e5c1643acd99006695166db
henriqueyun <henriqueyun@gmail.com>
2021-09-22 04:19:41 -0300

    first commit

---
+++ src/layouts/ChatLayout.vue
@@ -0,0 +1,52 @@
+<template>
+  <q-layout view="lHh lpr fFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+
+        <q-toolbar-title>
+          Quasar App
+        </q-toolbar-title>
+
+        <div>Quasar Doidera</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+    >
+      <q-list>
+        <q-item-label
+          header
+        >
+          Essential Links
+        </q-item-label>
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'ChatLayout',
+
+  setup () {
+    return {}
+  }
+})
+</script>
